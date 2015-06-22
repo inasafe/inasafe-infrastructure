a25cefce82e12229954e3a5339797d1845c95332
Hendrik vIsage <hvisage@envisage.co.za>
2015-06-22 23:19:24 +0200

    changes

--- ansible-hvt/init.sh
+++ ansible-hvt/init.sh
@@ -2,7 +2,7 @@
 
 #HV: This file is used for initial setup testing
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible -i init_inventory.ini -m ping vms --ask-pass --ask-sudo-pass -vv
+#ansible -i init_inventory.ini -m ping vms --ask-pass --ask-sudo-pass -vv
 #ansible-playbook -i init_inventory.ini --ask-pass -vv -s  -l vms users.yml --ask-sudo
 
 #ansible-playbook -i init_inventory.ini --ask-pass -vv -s  -l vms users.yml --ask-sudo
