c99f6c0a9fd36ffb9b4cdf07f32e3c6a52d6d5c0
hvisage@kartoza.co.za <hvisage@envisage.co.za>
2015-06-23 04:08:30 +0200

    Making progress with volumes mounted etc.

--- ansible-hvt/bootstrap.sh
+++ ansible-hvt/bootstrap.sh
@@ -5,11 +5,15 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo "Let's first check that we can connect okay with the VMs:"
 
-ansible -i bootstrap.ini -m ping vms --ask-pass -vvvv || exit 255
+#ansible -i bootstrap.ini -m ping vms --ask-pass -vvvv || exit 255
+ansible -i bootstrap.ini -m shell -a "pvs;lvs"  vms -b --ask-become-pass --ask-pass -vvvv || exit 255
 
 ansible-playbook -i bootstrap.ini --ask-pass -vv -b  -l vms bootstrap.yml --ask-become-pass 
-ansible -i inventory.ini -v  vms -m shell -a "/sbin/reboot" -e ansible_ssh_port=22 -b
+
+#At this point, we swap to the standard inventory :)
+
+ansible -i InaSafe.ini -v  all -m shell -a "/sbin/reboot" -e ansible_ssh_port=22 -b  || exit 127
 
 echo "Let's wait a tad:"
 for i in 10 9 8 7 6 5 4 3 2 1; do sleep 1;echo $i;done
-ansible -i inventory.ini -v  vms -m ping
+ansible -i InaSafe.ini -v  all -m ping  || exit 63
