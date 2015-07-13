288eeabca2d4481fac1197de1e8b9a85b376bc72
gavin <gavin@afrispatial.co.za>
2015-07-13 22:29:48 +0200

    removed ansible from master and renamed ansible-hvt to ansible; now master should contain only code that works in production; all development to take place in develop branch; Hendrik, if the vagrant-mgmt dir is working, please merge into ansible dir so that a user can choose to run playbooks against vagrant or a remote host of their choice

--- ansible/apply.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Determine avengers root directory
-DIR=$(cd `dirname $0` && pwd)
-
-# Invoke ansible
-ansible-playbook -i "${DIR}/production.ini" "${DIR}/site.yml" $*
--- ansible/host_vars/proxmox1.inasafe.org
+++
@@ -1,4 +0,0 @@
-
----
-ansible_ssh_host: 5.9.108.141
-ansible_ssh_user: root
